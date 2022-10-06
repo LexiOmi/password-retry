2ded7bef7681b77e2d0b6d00f17fdf0edd060e4d
Lexi Omi <lexinightx@gmail.com>
2022-10-06 19:14:05 +0800

    add some change :D

--- password-retry.py
+++ password-retry.py
@@ -5,10 +5,15 @@
 password = 'a123456'  #答案設在前面,以後要更改才不用到處改
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼: ')
-	if password != pwd:
-		i = i - 1
-		print('密碼錯誤! 還有', i, '次機會')
+	if password != pwd:	
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('沒機會嘗試了,要鎖帳號了~')
+
 	else:						#簡化
 		print('登入成功!')
 		break
