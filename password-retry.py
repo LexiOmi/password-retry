a2f9bfad9caed31d4baa365918bee2d40f244cae
Lexi Omi <lexinightx@gmail.com>
2022-10-06 19:06:36 +0800

    update, improve

--- password-retry.py
+++ password-retry.py
@@ -2,15 +2,14 @@
 # password: 'a123456'
 # 讓使用者輸入密碼,最多3次
 
+password = 'a123456'  #答案設在前面,以後要更改才不用到處改
 i = 3
 while i > 0:
-	password = input('請輸入密碼: ')
-	if password != 'a123456':
+	pwd = input('請輸入密碼: ')
+	if password != pwd:
 		i = i - 1
 		print('密碼錯誤! 還有', i, '次機會')
-	elif password == 'a123456':
+	else:						#簡化
 		print('登入成功!')
 		break
-
-
 
